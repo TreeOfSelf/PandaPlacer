e64274bee37fae975e917be80d91fa1e9b8ca80b
Sebastian <sebastian97@gmail.com>
2024-11-29 14:44:31 -0800

    Update PlacerItem.java

--- src/main/java/me/TreeOfSelf/PandaPlacer/PlacerItem.java
+++ src/main/java/me/TreeOfSelf/PandaPlacer/PlacerItem.java
@@ -2,14 +2,10 @@ package me.TreeOfSelf.PandaPlacer;
 
 import eu.pb4.polymer.core.api.item.PolymerItem;
 import net.minecraft.block.Block;
-import net.minecraft.component.DataComponentTypes;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.Text;
-import org.jetbrains.annotations.Nullable;
 import xyz.nucleoid.packettweaker.PacketContext;
 
 public class PlacerItem extends BlockItem implements PolymerItem {
@@ -17,8 +13,6 @@ public class PlacerItem extends BlockItem implements PolymerItem {
 
     public PlacerItem(Block block, Settings settings) {
         super(block, settings);
-        settings.component(DataComponentTypes.ITEM_NAME, Text.of("FUck"));
-
     }
 
 
